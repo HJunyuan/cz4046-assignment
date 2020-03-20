440789a0f91a364bcfd01d38600e2925c1aa9796
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-20 19:01:54 +0800

    Added more constants

--- assignment-1/src/entities/Constants.java
+++ assignment-1/src/entities/Constants.java
@@ -1,11 +1,20 @@
 package entities;
 
 public class Constants {
+	/* Grid World */
+	public final static int NUM_COL = 6;
+	public final static int NUM_ROW = 6;
+	
 	/* Reward */
 	public final static float REWARD_WHITE = -0.04f;
 	public final static float REWARD_GREEN = 1f;
 	public final static float REWARD_BROWN = -1f;
 	public final static float REWARD_WALL = 0f;
+	
+	/* Probabilities */
+	public final static float PROBABILITY_UP = 0.8f;
+	public final static float PROBABILITY_LEFT = 0.1f;
+	public final static float PROBABILITY_RIGHT = 0.1f;
 	
 	/* Discount Factor */
 	public final static float DISCOUNT_FACTOR = 0.99f;
