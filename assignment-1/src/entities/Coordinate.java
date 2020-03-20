4cc335e42243cb5241cbd11d9fb0f6b3886c409c
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-20 19:02:47 +0800

    Get neighbouring coordinates

--- assignment-1/src/entities/Coordinate.java
+++ assignment-1/src/entities/Coordinate.java
@@ -13,9 +13,14 @@ public class Coordinate {
 	public Coordinate(int col, int row) {
 		if (col < 0 || row < 0)
 			throw new IllegalArgumentException("Col and Row must be a positive integer.");
-
-		this.col = col;
-		this.row = row;
+		else if (col > Constants.NUM_COL - 1)
+			throw new IllegalArgumentException("Col out of range.");
+		else if (row > Constants.NUM_ROW - 1)
+			throw new IllegalArgumentException("Row out of range.");
+		else {
+			this.col = col;
+			this.row = row;
+		}
 	}
 
 	public int getCol() {
@@ -25,5 +30,40 @@ public class Coordinate {
 	public int getRow() {
 		return this.row;
 	}
-	
+
+	public Coordinate getUp() {
+		try {
+			Coordinate down = new Coordinate(this.col, this.row + 1);
+			return down;
+		} catch (IllegalArgumentException e) {
+			return this;
+		}
+	}
+
+	public Coordinate getDown() {
+		try {
+			Coordinate down = new Coordinate(this.col, this.row - 1);
+			return down;
+		} catch (IllegalArgumentException e) {
+			return this;
+		}
+	}
+
+	public Coordinate getLeft() {
+		try {
+			Coordinate down = new Coordinate(this.col - 1, this.row);
+			return down;
+		} catch (IllegalArgumentException e) {
+			return this;
+		}
+	}
+
+	public Coordinate getRight() {
+		try {
+			Coordinate down = new Coordinate(this.col + 1, this.row);
+			return down;
+		} catch (IllegalArgumentException e) {
+			return this;
+		}
+	}
 }
