13fba2c5b96b74b3aee4891ee0835201679334e8
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-26 15:03:00 +0800

    Introduced PolicyEvaluation

--- assignment-1/src/controllers/PolicyIteration.java
+++ assignment-1/src/controllers/PolicyIteration.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import entities.Cell;
+import entities.Constants;
+import entities.Coordinate;
 import entities.Grid;
+import entities.Policy;
 
 public class PolicyIteration {
 
@@ -13,7 +17,38 @@ public class PolicyIteration {
 		runPolicyIteration(grid);
 	}
 
-	public static void runPolicyIteration(Grid grid) {
+	private static void runPolicyIteration(Grid grid) {
+
+	}
+
+	/**
+	 * Calculates utilities for a given Policy.
+	 * 
+	 * @param grid
+	 * @return
+	 */
+	private static void policyEvaluation(Grid grid) {
+		for (int c = 0; c < Constants.NUM_COL; c++) {
+			for (int r = 0; r < Constants.NUM_ROW; r++) {
+				/* 1. Get current reward & policy */
+				Cell currCell = grid.getCell(new Coordinate(c, r));
+
+				/*
+				 * 2. Sum up the 3 neighbours (i.e. UP, LEFT, RIGHT) based on the current policy
+				 */
+				Cell[] neighbours = grid.getNeighboursOfCell(currCell);
+				double up = Constants.PROBABILITY_UP * neighbours[0].getUtility();
+				double left = Constants.PROBABILITY_LEFT * neighbours[1].getUtility();
+				double right = Constants.PROBABILITY_RIGHT * neighbours[2].getUtility();
+
+				/* 3. Update Utility */
+				float reward = currCell.getCellType().getReward();
+				currCell.setUtility(reward + up + left + right);
+			}
+		}
+	}
+
+	private static void policyImprovement() {
 
 	}
 }
