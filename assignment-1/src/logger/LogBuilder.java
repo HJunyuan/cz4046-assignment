ba3898d0d37962536fd27d57dac3c22ae386853f
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-28 18:57:17 +0800

    Completed normalisation

--- assignment-1/src/logger/LogBuilder.java
+++ assignment-1/src/logger/LogBuilder.java
@@ -2,6 +2,7 @@ package logger;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class LogBuilder {
 	private String fileName;
 	private String timestamp;
 	private List<String> headers;
-	private List<List<Double>> all_data;
+	private List<List<Double>> data;
 
 	public LogBuilder(String fileName, Grid grid) {
 		Date date = new Date();
@@ -23,7 +24,7 @@ public class LogBuilder {
 		this.fileName = fileName;
 		this.timestamp = dateFormat.format(date);
 		this.headers = new ArrayList<String>();
-		this.all_data = new ArrayList<List<Double>>();
+		this.data = new ArrayList<List<Double>>();
 
 		for (int c = 0; c < Constants.NUM_COL; c++) {
 			for (int r = 0; r < Constants.NUM_ROW; r++) {
@@ -44,10 +45,48 @@ public class LogBuilder {
 			}
 		}
 
-		this.all_data.add(incoming);
+		this.data.add(incoming);
 	}
 
 	public void finalise() {
-		CSV.writeToFile(this.fileName + "_" + this.timestamp, this.headers, this.all_data);
+		List<List<Double>> normalised = this.normalise(this.data);
+
+		CSV.writeToFile(this.fileName + "_" + this.timestamp, this.headers, normalised);
+	}
+
+	private List<List<Double>> normalise(List<List<Double>> incoming) {
+		List<List<Double>> normalised = new ArrayList<List<Double>>(incoming);
+
+		int numIterations = normalised.size();
+
+		for (int itr = 0; itr < numIterations; itr++) {
+			double posMax = this.findPosMax(itr);
+			double negMax = this.findNegMax(itr);
+
+			List<Double> states = normalised.get(itr);
+			for (int state = 0; state < states.size(); state++) {
+				Double currUtility = states.get(state);
+
+				if (currUtility > 0)
+					currUtility = currUtility / posMax;
+				else
+					currUtility = (-1) * currUtility / negMax;
+
+				if (currUtility.isNaN())
+					states.set(state, (double) 0);
+				else
+					states.set(state, currUtility);
+			}
+		}
+
+		return normalised;
+	}
+
+	private double findPosMax(int iteration) {
+		return Collections.max(this.data.get(iteration));
+	}
+
+	private double findNegMax(int iteration) {
+		return Collections.min(this.data.get(iteration));
 	}
 }
